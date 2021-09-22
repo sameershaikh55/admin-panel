018d4d1060b0a29f3d6d6e77c3bd4302c13dc234
sameer-shaikh <sameermubeen4@gmail.com>
2021-09-22 18:35:56 +0500

    login done

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { MdRemoveRedEye } from "react-icons/md";
+
+const LoginForm = () => {
+	return (
+		<div className="login_form pb-3">
+			<div className="container-fluid">
+				<div className="px-4">
+					<h4 className="f24 pt-4 fw600">Sign in</h4>
+					<p className="color2">Login to MoloDash</p>
+				</div>
+				<hr />
+
+				<form>
+					<div className="px-4">
+						<label htmlFor="Email">Email</label>
+						<br />
+						<input
+							className="w-100 px-3 mt-1 f14"
+							type="email"
+							placeholder="johndoe@gmail.com"
+						/>
+						<br />
+						<br />
+						<label htmlFor="Password">Password</label>
+						<br />
+						<div className="position-relative">
+							<input
+								className="w-100 px-3 mt-1 f14"
+								type="text"
+								placeholder="Password"
+							/>
+
+							<MdRemoveRedEye
+								className="eye pointer"
+								color="#90A0B7"
+								fontSize="1.1rem"
+							/>
+						</div>
+					</div>
+					<hr />
+
+					<div className="px-4">
+						<button className="w-100 border-0 text-white f14">Create</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default LoginForm;
